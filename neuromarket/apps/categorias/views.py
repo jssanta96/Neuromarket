63b15e8995c5006d333a31bca61b092b89ff8e7c
jsanta <jssanta96@gmail.com>
2019-09-12 00:54:43 -0500

    fix bug

--- neuromarket/apps/categorias/views.py
+++ neuromarket/apps/categorias/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
+from django.db.models import Subquery
 from .models import Categoria,SubCategoria
+from neuromarket.apps.productos.models import Producto 
+from neuromarket.apps.productos.serializers import productoSerializer 
 from .serializers import categoriaSerializer,subCategoriaSerializer
 from django.http import Http404
 
@@ -28,7 +31,8 @@ class FilterCategoria(APIView):
     def post(self,request):
         data = request.data
         subCategorias = SubCategoria.objects.filter(nombre__icontains=data['campo'])
-        subCategorias_json = subCategoriaSerializer(instance=subCategorias,many=True,context={"request": request})
+        producto = Producto.objects.filter(subcategoria_id__in=Subquery(subCategorias.values('id')))
+        subCategorias_json = productoSerializer(instance=producto,many=True,context={"request": request})
         return Response(subCategorias_json.data)
 
 
