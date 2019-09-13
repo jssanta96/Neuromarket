55d6675fc5b8af89d2387b2b87f4e6b1497a7532
DiegoBello <diego.bello@correounivalle.edu.co>
2019-09-13 00:39:28 -0500

    NMT-002 *cart delete

--- Frontend/neuromarket/src/app/views/cart/cart.component.ts
+++ Frontend/neuromarket/src/app/views/cart/cart.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
+  cartList;
+
   constructor() { }
 
   ngOnInit() {
+    this.initializeCart();
+  }
+
+  initializeCart(){
+    this.cartList = JSON.parse(localStorage.getItem('cart'))
+  }
+
+  removeItem(itemNumber){
+    this.cartList.splice(itemNumber,itemNumber);
+    localStorage.setItem('cart', JSON.stringify(this.cartList));
   }
 
 }
